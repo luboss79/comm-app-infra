fd422609e15f0882c61ed8ecb0554f74b05a36f5
Lubo Braso <lubomir.braso@posam.sk>
2021-10-13 15:04:08 +0200

    komentar pre vypnutie cron jobov

--- 06_crontab/get-crontab-file.sh
+++ 06_crontab/get-crontab-file.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo mkdir origin
 sudo cp /etc/crontab origin/
+sudo echo >> origin/crontab
+sudo echo "### Takymto komentarom su vypnute joby, ktore bude treba povolit po nainstalovani aplikacii" >> origin/crontab
