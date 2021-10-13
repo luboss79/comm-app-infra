6edefd7a9965f7538fdcc6449b49977d140e9e57
Lubo Braso <lubomir.braso@posam.sk>
2021-10-13 16:41:20 +0200

    pridane timezone, locale a selinux

---
+++ 00_system/set-system.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# timezone UTC
+sudo timedatectl set-timezone UTC
+
+# SELinux
+sudo setenforce 0
+sudo sed -i s/SELINUX=enforcing/SELINUX=permissive/g /etc/selinux/config
+
+# locale
+sudo cat << EOF > /etc/locale.conf
+LANG=en_US.UTF-8
+LC_PAPER=sk_SK.UTF-8
+LC_ADDRESS=sk_SK.UTF-8
+LC_MONETARY=sk_SK.UTF-8
+LC_NUMERIC=sk_SK.UTF-8
+LC_TELEPHONE=sk_SK.UTF-8
+LC_IDENTIFICATION=sk_SK.UTF-8
+LC_MEASUREMENT=sk_SK.UTF-8
+LC_NAME=sk_SK.UTF-8
+EOF
+
