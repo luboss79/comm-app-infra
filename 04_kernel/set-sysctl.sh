0f426542dce8bcd4e9b1f55bbd95f4c25dcfb0eb
Lubo Braso <lubomir.braso@posam.sk>
2021-10-18 13:35:00 +0200

    fix sysctl

--- 04_kernel/set-sysctl.sh
+++ 04_kernel/set-sysctl.sh
@@ -3,6 +3,7 @@ sudo cp origin/sysctl.conf /etc/sysctl.conf
 
 # Disable IPV6
 sudo sh -c 'echo >> /etc/sysctl.conf'
-sudo sh -c 'echo "Disable IPV6" >> /etc/sysctl.conf'
+sudo sh -c 'echo "# Disable IPV6" >> /etc/sysctl.conf'
 sudo sh -c 'echo "net.ipv6.conf.eth0.disable_ipv6 = 1" >> /etc/sysctl.conf'
 
+sudo sysctl -p
