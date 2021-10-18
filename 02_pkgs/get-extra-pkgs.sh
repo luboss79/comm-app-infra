dd1e633cb4df88a83737e424cad72c99faf45396
Lubo Braso <lubomir.braso@posam.sk>
2021-10-18 10:19:12 +0200

    odmazanie influxdb

--- 02_pkgs/get-extra-pkgs.sh
+++ 02_pkgs/get-extra-pkgs.sh
@@ -4,7 +4,6 @@ echo "#!/bin/bash" > install-extra-pkgs.sh
 epel_pkgs=$(yum list installed | grep @epel | grep -v nagios | awk '{print $1}' | awk 'BEGIN { ORS = " " } { print }')
 remi_pkgs=$(yum list installed | grep @remi | awk '{print $1}' | awk 'BEGIN { ORS = " " } { print }')
 percona_pkgs=$(yum list installed | grep @percona | awk '{print $1}' | awk 'BEGIN { ORS = " " } { print }')
-influxdb_pkgs=$(yum list installed | grep @influxdb | awk '{print $1}' | awk 'BEGIN { ORS = " " } { print }')
 ius_pkgs=$(yum list installed | grep @ius | awk '{print $1}' | awk 'BEGIN { ORS = " " } { print }')
 
 # EPEL pkgs
@@ -15,9 +14,6 @@ echo "sudo yum install -y $remi_pkgs" >> install-extra-pkgs.sh
 
 # Mysql pkgs
 echo "sudo yum install -y $percona_pkgs" >> install-extra-pkgs.sh
-
-# Influxdb - telegraf pkgs
-echo "sudo yum install -y $influxdb_pkgs" >> install-extra-pkgs.sh
 
 # IUS - mongodb
 echo "sudo yum install -y $ius_pkgs" >> install-extra-pkgs.sh
