adb15d4b7438b306bd18bbea168834cecd733710
Lubo Braso <lubomir.braso@posam.sk>
2021-10-18 10:25:03 +0200

    fix sudo - presmerovanie do fajlu

--- 04_kernel/set-sysctl.sh
+++ 04_kernel/set-sysctl.sh
@@ -2,7 +2,7 @@
 sudo cp origin/sysctl.conf /etc/sysctl.conf
 
 # Disable IPV6
-echo >> /etc/sysctl.conf
-echo "Disable IPV6" >> /etc/sysctl.conf
-echo "net.ipv6.conf.eth0.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo sh -c 'echo >> /etc/sysctl.conf'
+sudo sh -c 'echo "Disable IPV6" >> /etc/sysctl.conf'
+sudo sh -c 'echo "net.ipv6.conf.eth0.disable_ipv6 = 1" >> /etc/sysctl.conf'
 
