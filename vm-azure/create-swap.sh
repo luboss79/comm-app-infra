e009573a038aadfaab4187dac885049b120c3fc2
Lubo Braso <lubomir.braso@posam.sk>
2021-10-21 11:08:03 +0200

    fix swap

--- vm-azure/create-swap.sh
+++ vm-azure/create-swap.sh
@@ -5,7 +5,7 @@ sudo dd if=/dev/zero of=/mnt/swapfile bs=1024 count=4194304
 sudo chmod 600 /mnt/swapfile 
 sudo mkswap /mnt/swapfile 
 sudo swapon /mnt/swapfile 
-sudo bash -c '"echo /mnt/swapfile	swap	swap    defaults        0 0" >> /etc/fstab'
+sudo bash -c "echo $'/mnt/swapfile\tswap\tswap\tdefaults\t0\t0' >> /etc/fstab"
 sudo cat /etc/fstab
 echo
 free
